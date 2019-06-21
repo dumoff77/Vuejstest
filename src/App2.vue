aed840530cca1803ed140936005bbcd977ee8ff7
dumoff77 <sirenchenkod@gmail.com>
2019-06-21 11:42:20 +0300

    Vue3

---
+++ src/App2.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+    <el-button type="text" @click="dialogVisible = true">click to open the Dialog</el-button>
+
+    <el-dialog
+            title=""
+            :visible.sync="dialogVisible"
+            width="30%"
+            :before-close="handleClose">
+        <div>
+            <div class="demo-input-label">
+                <span>Login: </span>
+            </div>
+            <div class="demo-input-label">
+                <el-input placeholder="login" v-model="login"></el-input>
+            </div>
+        </div>
+
+
+        <div>
+            <div class="demo-input-label">
+                <span>Pass: </span>
+            </div>
+            <div class="demo-input-label">
+                <el-input placeholder="password" v-model="pass"></el-input>
+            </div>
+        </div>
+
+        <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Ок</el-button>
+        <el-button type="primary" @click="dialogVisible = false">Отмена</el-button>
+      </span>
+    </el-dialog>
+    </div>
+</template>
+
+<style>
+    .demo-input-label {
+        display: inline-block;
+        width: 130px;
+    }
+</style>
+
+<script>
+    export default {
+        data() {
+            return {
+                dialogVisible: false,
+                login: '',
+                pass: ''
+            };
+        },
+        methods: {
+            handleClose(done) {
+                this.$confirm('Are you sure to close this dialog?')
+                    .then(_ => {
+                        done();
+                    })
+                    .catch(_ => {});
+            }
+        }
+    };
+</script>
