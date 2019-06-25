998f0c02e83b8004c337bc364b6f824846ad0bc7
dumoff77 <sirenchenkod@gmail.com>
2019-06-25 15:13:26 +0300

    page autoupdate

--- src/table.vue
+++ src/table.vue
@@ -34,15 +34,15 @@
                 <template slot-scope="scope">
                     <el-button @click="DeleteRecordBuffer = scope, DeleteDialogVisible = true" type="text" size="small">Delete</el-button>
                     <el-button @click="InputDialogVisible = true,
-                    PostForm.first_name = scope.row.name,
-                    PostForm.tel_number = scope.row.number,
-                    PostForm.desc = scope.row.description" type="text" size="small">Edit</el-button>
+                    PostForm.name = scope.row.name,
+                    PostForm.number = scope.row.number,
+                    PostForm.description = scope.row.description" type="text" size="small">Edit</el-button>
                 </template>
             </el-table-column>
         </el-table>
 
         <el-button type="primary" @click="InputDialogVisible = true,
-        PostForm.first_name = PostForm.tel_number = PostForm.desc = ''">Add new record</el-button>
+        PostForm.name = PostForm.number = PostForm.description = ''">Add new record</el-button>
         <el-dialog
                 title=""
                 :visible.sync="InputDialogVisible"
@@ -50,21 +50,21 @@
                 :before-close="handleClose">
             <el-form :model="PostForm" status-icon ref="ruleForm" label-width="120px" class="demo-ruleForm">
                 <el-form-item label="Имя" prop="first_name">
-                    <el-input type="login" v-model="PostForm.first_name" autocomplete="off"></el-input>
+                    <el-input type="login" v-model="PostForm.name" autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="Телефон" prop="tel_number">
-                    <el-input type="tel_number" v-model="PostForm.tel_number" autocomplete="off"></el-input>
+                    <el-input type="tel_number" v-model="PostForm.number" autocomplete="off"></el-input>
                 </el-form-item>
                 <el-form-item label="Описание" prop="desc">
-                    <el-input type="desc" v-model="PostForm.desc" autocomplete="off"></el-input>
+                    <el-input type="desc" v-model="PostForm.description" autocomplete="off"></el-input>
                 </el-form-item>
             </el-form>
 
             <span slot="footer" class="dialog-footer">
                 <el-button @click="InputDialogVisible = false,
-                UpdateHandbook(PostForm.first_name,PostForm.tel_number,PostForm.desc)">Обновить</el-button>
+                UpdateHandbook()">Обновить</el-button>
                 <el-button :visible.sync="InputButtonVisible" @click="InputDialogVisible = false,
-                SetHandbook(PostForm.first_name,PostForm.tel_number,PostForm.desc)">Сохранить</el-button>
+                SetHandbook()">Сохранить</el-button>
                 <el-button type="primary" @click="InputDialogVisible = false">Отмена</el-button>
             </span>
         </el-dialog>
@@ -75,7 +75,7 @@
                 width="30%"
                 :before-close="handleClose">
             <span slot="footer" class="dialog-footer">
-                <el-button @click="deleteHandbook(DeleteRecordBuffer.row.name), DeleteDialogVisible = false" type="success" size="medium">Delete</el-button>
+                <el-button @click="deleteHandbook(), DeleteDialogVisible = false" type="success" size="medium">Delete</el-button>
                 <el-button type="danger" size="medium" @click="DeleteDialogVisible = false">Отмена</el-button>
             </span>
         </el-dialog>
@@ -93,36 +93,37 @@
                 InputDialogVisible: false,
                 DeleteDialogVisible: false,
                 PostForm: {
-                    first_name: '',
-                    tel_number: '',
-                    desc: ''
+                    name: '',
+                    number: '',
+                    description: ''
                 },
-                tableData: [{
+                tableData: [
+                    {
                     number: '0980446512',
                     name: 'Valera',
                     description: 'best friend'
-                }, {
+                },
+                    {
                     number: '0675487457',
                     name: 'Vasya',
                     description: 'classmate'
-                }]
+                }
+                ]
             }
         },
         methods: {
-            UpdatePage() {
-                axios.get('/api/handbook').then(response => {
-                    this.tableData = response.data;
-                })
+            UpdateHandbook() {
+                axios.put('/api/handbook/'+this.PostForm.name ,this.PostForm);
+                this.tableData.pop(this.PostForm);
+                this.tableData.push(this.PostForm);
             },
-            UpdateHandbook(first_name,tel_number,desc) {
-                console.log(first_name,tel_number,desc)
-                axios.put('/api/handbook/'+first_name,{ "name":first_name, "number":tel_number, "description":desc })
+            SetHandbook() {
+                axios.post('/api/handbook',this.PostForm);
+                this.tableData.push(this.PostForm);
             },
-            SetHandbook(first_name,tel_number,desc) {
-                axios.post('/api/handbook',{ "name":first_name, "number":tel_number, "description":desc })
-            },
-            deleteHandbook(mydata) {
-                axios.delete('/api/handbook/'+mydata);
+            deleteHandbook() {
+                axios.delete('/api/handbook/'+this.DeleteRecordBuffer.row.name);
+                this.tableData.pop(this.DeleteRecordBuffer.row);
             },
             handleClose(done) {
                 this.$confirm('Are you sure to close this dialog?')
